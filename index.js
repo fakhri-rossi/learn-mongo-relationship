4be8ad1ec8b5e0120f743fc7ba53226691dfe71e
Fakhri Rossi <rossifakhri@gmail.com>
2024-06-17 20:22:48 +0700

    One to Few Relation

--- index.js
+++ index.js
@@ -1,16 +1,64 @@
-const express = require('express');
+// const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
+// const app = express();
 
-mongoose.connect('mongodb://127.0.0.1/shop_db')
+mongoose.connect('mongodb://127.0.0.1/relation_db')
     .then((result) => {
-        console.log(result);
+        console.log('connected to mongodb');
 
     }).catch((err) => {
         console.log(err);
 
     });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    addresses: [{
+        _id: false,
+        street: String,
+        city: String,
+        country: String
+    }]
 });
+
+const User = mongoose.model('User', userSchema);
+
+// === One to Few ===
+// one user has few addresses (stored in same document)
+const makeUser = async() => {
+    const user = new User({
+        name: 'Rossi'
+    });
+
+    user.addresses.push({
+        street: 'Jl. Pandanwangi',
+        city: 'Bandung',
+        country: 'Indonesia'
+    });
+
+    const res = await user.save();
+
+    console.log(res);
+};
+
+// makeUser();
+
+const addAddress = async (id) => {
+    const user = await User.findById(id);
+
+    user.addresses.push({
+        street: "Jl Bintara 12A",
+        city: 'Bekasi',
+        country: 'Indonesia'
+    });
+
+    const res = await user.save();
+    console.log(res);
+};
+
+// addAddress('6670359b2495cab2b8cf965d');
+
+// app.listen(3000, () => {
+//     console.log('Server is running on port 3000');
+// });
