643270c06509c14a7cd548336e780392c91d94f2
Fakhri Rossi <rossifakhri@gmail.com>
2024-06-17 19:52:39 +0700

    fixed bug

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:shop_db')
+mongoose.connect('mongodb://127.0.0.1/shop_db')
     .then((result) => {
         console.log(result);
 
