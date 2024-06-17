db76e9dd1fdfafa3bbcea60dce0d5e22bfc552ad
Fakhri Rossi <rossifakhri@gmail.com>
2024-06-17 19:51:08 +0700

    Project preparation

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:shop_db')
+    .then((result) => {
+        console.log(result);
+
+    }).catch((err) => {
+        console.log(err);
+
+    });
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
